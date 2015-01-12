6164bd1abe2c91bfcea15f4a386a009fe7011294
tcleyman <tcleyman@TCLEYMAN-LAP.be.oracle.com>
2015-01-12 15:25:13 +0100

    added WLST facet for scripting

---
+++ wlst/newWlst.py
@@ -0,0 +1,32 @@
+
+#Conditionally import wlstModule only when script is executed with jython
+if __name__ == '__main__': 
+    from wlstModule import *#@UnusedWildImport
+
+def getRunningServerNames():
+    domainConfig()
+    return cmo.getServers()
+
+def monitorRunningServers():
+    print 'starting the script ....'
+    username = 'weblogic'
+    password = 'welcome1'
+    url='t3://localhost:7001'
+    connect(username,password,url)
+    
+    serverNames = getRunningServerNames()
+    domainRuntime()
+    for name in serverNames:
+        print 'Now checking '+name.getName()
+        try:
+            cd("/ServerRuntimes/"+name.getName())
+        except WLSTException,e:
+            # this typically means the server is not active, just ignore
+            print e
+            pass
+        print cmo.getName()
+        print cmo.getSocketsOpenedTotalCount()
+
+if __name__== "main":
+    monitorRunningServers()
+    
